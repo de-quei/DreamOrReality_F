971ac6b0db9c5a316621e2de2992aef980ac98ca
Hyunji Kim <s2208@e-mirim.hs.kr>
2023-10-25 16:21:45 +0900

    랜덤으로 문구가 나오게

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+// cList 배열 정의
 let cList = [
     "크게 칭찬하고 작은 소리로 비난하라.",
     "내 스스로 확신한다면 나는 남의 확신을 구하지 않는다.",
@@ -6,3 +7,11 @@ let cList = [
     "생각하는 대로 살지 않으면 사는 대로 생각하게 된다.",
     "고개를 들어라. 각도가 곧 태도다."
 ];
+
+// 랜덤으로 인용구 선택
+let randomIndex = Math.floor(Math.random() * cList.length);
+let randomCompliment = cList[randomIndex];
+
+// compliment-div에 텍스트 추가
+let complimentDiv = document.querySelector(".compliment-div");
+complimentDiv.textContent = randomCompliment;
